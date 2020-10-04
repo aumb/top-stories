8438a103c9e7b8687d7ef03eedc6a522a62b362b
Mathiew Abbas <mathiew.abbas@ubanquity.com>
2020-10-04 20:02:05 +0300

    fix

--- lib/core/utils/strings.dart
+++ lib/core/utils/strings.dart
@@ -6,10 +6,10 @@ class Strings {
   static final String genericError = "An error has occured";
   static final String retry = "Retry";
   static final String noData = "No articles have been found!";
-  static final String bookmark = "Add Bookmark";
+  static final String bookmark = "Add bookmark";
   static final String fullArticle = "View full article";
   static final String yourBookmarks = "Your Bookmarks";
-  static final String removeBookmark = "Remove Bookmark";
+  static final String removeBookmark = "Remove bookmark";
   static final String removedBookmark = "Removed bookmark";
   static final String undo = "Undo";
   static final String addedToBookmarks = "Added to bookmarks";
